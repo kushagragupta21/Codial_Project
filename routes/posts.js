4a29159e2356e9c87bd01403ddcfb862876c372b
Kushagra <kushagragupta6038@gmail.com>
2020-08-18 14:06:06 +0530

    created post form page

--- routes/posts.js
+++ routes/posts.js
@@ -5,8 +5,8 @@ const router = express.Router();
 const postController = require('../controllers/post_controlller');
 const passport = require('passport');
 
-router.post('/create' , postController.create);
-
-
+router.post('/create', passport.checkAuthentication , postController.create);
+//checkAuthentication is a function which we will created at config/passport-local-stretegy.js
+// 
 
 module.exports =  router;
